c9446ad2ab596e676d2be292f94a1d04bee06da0
Eric Nantz <thercast@gmail.com>
2019-03-25 00:44:54 -0400

    add draft of shinydevseries ep 0 slides

---
+++ r_scripts/fontawesome_scratch.R
@@ -0,0 +1,5 @@
+# if I put this in the author field of slides, the icons appear but we have some silly text at the top
+
+"Eric Nantz<br>`r icon::fa(name = 'twitter')` @thercast<br> `r icon::fa(name = 'github')` <a href='https://github.com/thercast'>github.com/thercast</a><br> `r icon::fa(name = 'microphone')` <a href='https://r-podcast.org'>r-podcast.org</a>"
+
+"/home/eric/R/x86_64-pc-linux-gnu-library/3.5/icon/fonts/font-awesome-5.0.12/css/fontawesome-all.min.css"
